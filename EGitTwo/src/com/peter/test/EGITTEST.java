d8c7a0101898d433fad079b970cb7cf9736cf491
peter <peterdevemail@gmail.com>
2013-11-02 16:43:11 -0400

    branch 1 sencond update

--- EGitTwo/src/com/peter/test/EGITTEST.java
+++ EGitTwo/src/com/peter/test/EGITTEST.java
@@ -5,6 +5,7 @@ public class EGITTEST {
 	public static void main(String[] args) {
 		System.out.println("This is the egit 2 Updated");
 		System.out.println("new branch 1 is created");
+		System.out.println("new branch 1 second update");
 
 	}
 
