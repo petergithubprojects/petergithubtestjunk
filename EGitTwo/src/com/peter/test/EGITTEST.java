0bafaa76da6a05e747cb409b4717dce36783f824
peter <peterdevemail@gmail.com>
2013-11-02 15:40:44 -0400

    Update 1 from egit2

--- EGitTwo/src/com/peter/test/EGITTEST.java
+++ EGitTwo/src/com/peter/test/EGITTEST.java
@@ -3,7 +3,7 @@ package com.peter.test;
 public class EGITTEST {
 
 	public static void main(String[] args) {
-		System.out.println("This is the egit 2");
+		System.out.println("This is the egit 2 Updated");
 
 	}
 
